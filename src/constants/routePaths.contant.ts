db2e951443ca77b4ba8dd1b389bd44e8e0e373fc
eltaeif-houssem <houssem.developer@gmail.com>
2024-09-18 23:51:15 +0100

    add auth route paths

---
+++ src/constants/routePaths.contant.ts
@@ -0,0 +1,4 @@
+// auth route paths
+export const AUTH_SIGNUP = "/auth/signup";
+export const AUTH_SIGNIN = "/auth/signin";
+export const AUTH_FORGET_PASSWORD = "/auth/forget-password";
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,23 @@
 import { lazy } from "react";
 import { RouteObject } from "react-router-dom";
+import * as routePaths from "@constants/routePaths.contant";
 
+// import auth pages
 const Signin = lazy(() => import("@pages/auth/Signin"));
 const Signup = lazy(() => import("@pages/auth/Signup"));
 const ForgetPassword = lazy(() => import("@pages/auth/ForgetPassword"));
 
 export const router: RouteObject[] = [
   {
-    path: "/auth/signin",
+    path: routePaths.AUTH_SIGNIN,
     element: <Signin />,
   },
-
   {
-    path: "/auth/signup",
+    path: routePaths.AUTH_SIGNUP,
     element: <Signup />,
   },
-
   {
-    path: "/auth/forget-password",
+    path: routePaths.AUTH_FORGET_PASSWORD,
     element: <ForgetPassword />,
   },
 ];
