5236cf863f633ebb82deaed63ad02e825241c218
eltaeif-houssem <houssem.developer@gmail.com>
2024-09-20 21:32:28 +0100

    adjust layout for both profile and saves pages

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "@styles/pages/profile/profile.css";
+import Layout from "@/components/layout";
 
 const Profile: React.FC = () => {
-  return <div className="profile-page">index</div>;
+  return (
+    <Layout>
+      <div className="profile-page">index</div>
+    </Layout>
+  );
 };
 
 export default Profile;
--- src/pages/saves/index.tsx
+++ src/pages/saves/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "@styles/pages/saves/saves.css";
+import Layout from "@/components/layout";
 
 const Saves: React.FC = () => {
-  return <div className="saves-page">index</div>;
+  return (
+    <Layout>
+      <div className="saves-page">index</div>
+    </Layout>
+  );
 };
 
 export default Saves;
