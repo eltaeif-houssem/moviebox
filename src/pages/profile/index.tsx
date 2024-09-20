7a64e977897647b885a5a79f4a80fa4a55bd32e1
eltaeif-houssem <houssem.developer@gmail.com>
2024-09-20 21:37:33 +0100

    secure profile and saves pages

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "@styles/pages/profile/profile.css";
 import Layout from "@/components/layout";
+import { appContext } from "@/context";
+import { Navigate } from "react-router-dom";
+import * as routePaths from "@constants/routePaths.contant";
 
 const Profile: React.FC = () => {
+  const context = appContext();
+
+  if (!context.user) {
+    <Navigate to={routePaths.HOME_PAGE} />;
+  }
   return (
     <Layout>
       <div className="profile-page">index</div>
--- src/pages/saves/index.tsx
+++ src/pages/saves/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "@styles/pages/saves/saves.css";
 import Layout from "@/components/layout";
+import { appContext } from "@/context";
+import { Navigate } from "react-router-dom";
+import * as routePaths from "@constants/routePaths.contant";
 
 const Saves: React.FC = () => {
+  const context = appContext();
+
+  if (!context.user) {
+    <Navigate to={routePaths.HOME_PAGE} />;
+  }
   return (
     <Layout>
       <div className="saves-page">index</div>
