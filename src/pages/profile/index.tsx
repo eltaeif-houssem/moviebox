53bd04646301976b5d5de6a21f767eb755468a65
eltaeif-houssem <houssem.developer@gmail.com>
2024-09-20 21:27:46 +0100

    add saves and profile pages

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "@styles/pages/profile/profile.css";
+
+const Profile: React.FC = () => {
+  return <div className="profile-page">index</div>;
+};
+
+export default Profile;
---
+++ src/pages/saves/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "@styles/pages/saves/saves.css";
+
+const Saves: React.FC = () => {
+  return <div className="saves-page">index</div>;
+};
+
+export default Saves;
