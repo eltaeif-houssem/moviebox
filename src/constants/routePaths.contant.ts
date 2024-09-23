014ba085468bcd8308a0fad99d535e1dee5db1d3
eltaeif-houssem <houssem.developer@gmail.com>
2024-09-23 10:03:27 +0100

    add route paths

--- src/constants/routePaths.contant.ts
+++ src/constants/routePaths.contant.ts
@@ -3,6 +3,14 @@ export const AUTH_SIGNUP_PAGE = "/auth/signup";
 export const AUTH_SIGNIN_PAGE = "/auth/signin";
 export const AUTH_FORGET_PASSWORD_PAGE = "/auth/forget-password";
 
+// movie route paths
+export const MOVIES_PAGE = "/movies";
+export const MOVIE_DETAILS_PAGE = "/movies/:id";
+
+// tv route paths
+export const TVS_PAGE = "/tvs";
+export const TV_DETAILS_PAGE = "/tv/:id";
+
 // other route paths
 export const HOME_PAGE = "/";
 export const PROFILE_PAGE = "/profile";
