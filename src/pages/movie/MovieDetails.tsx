5cdc1139e541211e751ec3f222aaec9907981203
eltaeif-houssem <houssem.developer@gmail.com>
2024-09-25 10:30:50 +0100

    add movie details page and the css file

--- src/pages/movie/MovieDetails.tsx
+++ src/pages/movie/MovieDetails.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Layout from "@/components/layout";
+import { appContext } from "@/context";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "@styles/pages/movie/movieDetails.css";
 
 const MovieDetails: React.FC = () => {
-  return <div>MovieDetails</div>;
+  const context = appContext();
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {};
+
+    fetchData();
+  }, []);
+  return (
+    <Layout dark={true}>
+      <div className="movie-details-page">hey</div>
+    </Layout>
+  );
 };
 
 export default MovieDetails;
